e8586193db090340b4ae87ca75c69d42497d33ca
Tong <163929652+Tong-Ye-1159668@users.noreply.github.com>
2024-06-11 16:42:14 +1200

    Added comment, use more bootstrap

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def getCursor():
 def home():
     return render_template("base.html")
 
+# Display a list of campers who are camping on a particular night
 @app.route("/campers", methods=['GET','POST'])
 def campers():
     if request.method == "GET":
@@ -36,12 +37,16 @@ def campers():
     else:
         campDate = request.form.get('campdate')
         connection = getCursor()
+
+        # Query to retrieve camper information for the particular date
         query = ('''SELECT customers.customer_id, customers.firstname, customers.familyname, customers.email, customers.phone
                     FROM bookings
                     INNER JOIN customers ON bookings.customer = customers.customer_id
                     WHERE bookings.booking_date = %s''')
         connection.execute(query, (campDate,))
         camperList = connection.fetchall()
+
+        # Render the camper list with the particular camp date
         return render_template("camperlist.html", camperlist = camperList, campdate=campDate)
     
 @app.route("/bookinglist", methods = ['GET'])
@@ -51,6 +56,8 @@ def bookinglist():
     bookingList = connection.fetchall()
     return render_template("currentbooking.html", bookinglist = bookingList)
 
+
+# Display a booking form to book a site
 @app.route("/booking", methods=['GET','POST'])
 def booking():
     if request.method == "GET":
@@ -59,103 +66,125 @@ def booking():
         bookingNights = request.form.get('bookingnights')
         bookingDate = request.form.get('bookingdate')
         occupancy = request.form.get('occupancy')
-        firstNight = date.fromisoformat(bookingDate)
 
+        # Calculate the last night of the booking
+        firstNight = date.fromisoformat(bookingDate)
         lastNight = firstNight + timedelta(days=int(bookingNights))
+
         connection = getCursor()
         connection.execute("SELECT * FROM customers;")
         customerList = connection.fetchall()
+
+        # Query to retrieve available sites for the specified occupancy and date range
         connection.execute("select * from sites where occupancy >= %s AND site_id not in (select site from bookings where booking_date between %s AND %s);",(occupancy,firstNight,lastNight))
         siteList = connection.fetchall()
+
+        # Render the booking form with all customer list, available sites, and booking details
         return render_template("bookingform.html", customerlist = customerList, bookingdate=bookingDate, sitelist = siteList, bookingnights = bookingNights, occupancy = occupancy)    
 
+
+# Add a booking to the database
 @app.route("/booking/add", methods=['POST'])
 def makebooking():
-    customer_id = request.form.get('customer')
-    site_id = request.form.get('site')
-    booking_date = request.form.get('bookingdate')
-    booking_nights = request.form.get('bookingnights')
+    customerId = request.form.get('customer')
+    siteId = request.form.get('site')
+    bookingDate = request.form.get('bookingdate')
+    bookingNights = request.form.get('bookingnights')
     occupancy = request.form.get('occupancy')
 
-    booking_nights = int(booking_nights)
-
-    first_night = date.fromisoformat(booking_date)
+    bookingnights = int(bookingNights)
+    firstNight = date.fromisoformat(bookingDate)
     
     connection = getCursor()
 
     # Insert each night of the booking into the database
-    for night in range(booking_nights):
-        current_night = first_night + timedelta(days=night)
+    for night in range(bookingnights):
+        current_night = firstNight + timedelta(days=night)
         query = "INSERT INTO bookings (site, customer, booking_date, occupancy) VALUES (%s, %s, %s, %s)"
-        connection.execute(query, (site_id, customer_id, current_night, occupancy))
+        connection.execute(query, (siteId, customerId, current_night, occupancy))
 
     # Fetch customer details for confirmation
     connection = getCursor()
-    connection.execute("SELECT firstname, familyname FROM customers WHERE customer_id = %s", (customer_id,))
+    connection.execute("SELECT firstname, familyname FROM customers WHERE customer_id = %s", (customerId,))
     customer = connection.fetchone()
 
-    customer_name = f"{customer[0]} {customer[1]}"
-    end_date = first_night + timedelta(days=booking_nights - 1)
+    customerName = f"{customer[0]} {customer[1]}"
 
-    return render_template("booking_confirmation.html", customer=customer_name, site=site_id, start_date=first_night, end_date=end_date, occupancy=occupancy)
+    # Calculate the check out date of the booking
+    lastNight = firstNight + timedelta(days=bookingnights)
+
+    # Render the booking confirmation page with the booking details
+    return render_template("booking_confirmation.html", customername=customerName, siteid=siteId, firstnight=firstNight, lastnight=lastNight, occupancy=occupancy)
 
 
-
+# Display a list of all campers
 @app.route("/allcamperlist", methods = ['GET'])
 def allcamperlist():
     connection = getCursor()
     connection.execute("SELECT * FROM customers;")
-    allcamperList = connection.fetchall()
-    return render_template("allcamperlist.html", allcamperlist = allcamperList)
+    allCamperList = connection.fetchall()
+    return render_template("allcamperlist.html", allcamperlist = allCamperList)
 
+
+# Add a new customer to the database
 @app.route("/addcustomer", methods=['GET', 'POST'])
 def addcustomer():
-    new_id = request.form.get('customer_id') 
-    firstname = request.form.get('firstname') 
-    familyname = request.form.get('familyname') 
+    newId = request.form.get('customer_id') 
+    firstName = request.form.get('firstname') 
+    familyName = request.form.get('familyname') 
     email = request.form.get('email')
     phone = request.form.get('phone')
     if request.method == "GET":
         return render_template("addcustomer.html") 
     else:
         connection = getCursor()
-        connection.execute('SELECT MAX(customer_id) FROM customers')
+        connection.execute("SELECT MAX(customer_id) FROM customers")
         result = connection.fetchone()
         max_customer_id = result[0] if result[0] is not None else 0
-        new_id = max_customer_id + 1
-        connection.execute('INSERT INTO customers (customer_id, firstname, familyname, email, phone) VALUES (%s,%s,%s,%s,%s);', (new_id, firstname, familyname, email, phone,))
+        newId = max_customer_id + 1
+        # Insert the new customer into the database
+        connection.execute("INSERT INTO customers (customer_id, firstname, familyname, email, phone) VALUES (%s,%s,%s,%s,%s);", (newId, firstName, familyName, email, phone,))
+        # Fetch the new customer details to the whole customer list
         return redirect("/allcamperlist")
 
+
+# Search for a customer by name, including partial text matches
 @app.route('/search',  methods=["GET","POST"])
 def search():
     if request.method == "GET":
         return render_template("search.html")
     else:
-        search_Name = request.form['search_Name']
+        searchName = request.form['searchName']
         connection = getCursor()
-        connection.execute("SELECT * FROM customers WHERE firstname LIKE %s OR familyname LIKE %s;",('%' + search_Name + '%', '%' + search_Name + '%'))
+
+        # Query to search for customers by first name or family name
+        connection.execute("SELECT * FROM customers WHERE firstname LIKE %s OR familyname LIKE %s;",('%' + searchName + '%', '%' + searchName + '%'))
         resultList = connection.fetchall()
-        return render_template("search_results.html", resultlist=resultList, search_name=search_Name)
-    
+
+        # Render the search results with the search name and result list
+        return render_template("search_results.html", resultlist=resultList, searchname=searchName)
+
+
+# Edit a customer's details
 @app.route('/edit_customer/<customer_id>', methods=["GET", "POST"])
 def edit_customer(customer_id):
+    # Retrieve the customer details for editing
     if request.method == "GET":
         connection = getCursor()
         connection.execute("SELECT * FROM customers WHERE customer_id = %s", (customer_id,))
         customerDetail = connection.fetchone()
         return render_template('edit_customer.html', customerdetail=customerDetail)
+    # Update the customer details in the database
     elif request.method == "POST":
-        new_firstname = request.form['firstname']
-        new_familyname = request.form['familyname']
-        new_email = request.form['email']
-        new_phone = request.form['phone']
+        newFirstName = request.form['firstname']
+        newFamilyName = request.form['familyname']
+        newEmail = request.form['email']
+        newPhone = request.form['phone']
 
         connection = getCursor()
-        query = """
-        UPDATE customers
-        SET firstname = %s, familyname = %s, email = %s, phone = %s
-        WHERE customer_id = %s
-        """
-        connection.execute(query, (new_firstname, new_familyname, new_email, new_phone, customer_id))
+        # Update the customer details in the database
+        query = "UPDATE customers SET firstname = %s, familyname = %s, email = %s, phone = %s WHERE customer_id = %s;"
+        connection.execute(query, (newFirstName, newFamilyName, newEmail, newPhone, customer_id))
 
+        # Redirect to the list of all campers after updating the customer details
         return redirect("/allcamperlist")
